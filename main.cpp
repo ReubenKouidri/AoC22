83e73ddaa935782b33a32061c7fc1d91ace6713c
rkouidri <reuben-kouidri@hotmail.com>
2022-12-12 19:25:05 +0000

    cleanup

--- main.cpp
+++ main.cpp
@@ -1,83 +1,52 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
-#include <algorithm>
+#include <vector>
 
 
-template<typename T>
-void printVector(const std::vector<T>& vec){
-    for (const auto i : vec){
-        std::cout << ' ' << i << ' ';
-    }
-    std::cout << '\n';
-}
-
-template<typename T>
-void moveItems(size_t num, std::vector<T> &fromStack, std::vector<T> &toStack)
+bool repeat(std::string& str)
 {
-    for (size_t i = num; i > 0; i--){
-        toStack.emplace_back(fromStack.back());
-        fromStack.pop_back();
-    }
-}
-
-/*
-
-int main()
-{
-    const size_t numStacks = 9;
-    std::ifstream cratesFile("inputs/d5.txt");
-    std::string cratesLine;
-    std::vector<char> crates[numStacks];
-
-    while (getline(cratesFile, cratesLine)) {
-        if (cratesLine.substr(1, 1) == "1") break;
-        for (size_t i = 0; i < numStacks; i++) {
-            if (cratesLine.substr(4*i + 1, 1) == " ") continue;
-            crates[i].push_back(cratesLine[4*i + 1]);
+    for (int i = 0; i < str.length(); i++){
+        for (int j = 0; j < str.length(); j++){
+            if (str[i] == str[i + j]){
+                return true;
+            }
         }
     }
-
-    for (size_t i = 0; i < numStacks; i++)
-        std::reverse(crates[i].begin(), crates[i].end());
-
-    size_t num, from, to;
-    std::string blank;
-    while (cratesFile >> blank >> num >> blank >> from >> blank >> to) {
-        move(num, crates[from - 1], crates[to - 1]);
-    }
-
-    for (size_t i = 0; i < numStacks; i++)
-        std::cout << crates[i].back();
-    std::cout << "\n";
+    return false;
 }
-
-*/
 
 int main()
 {
-    std::string filename{"/Users/juliettekouidri/Documents/Reuben/Projects/Cpp/AoC22/day5/day5_test.txt"};
-    std::ifstream file {};
+    std::string filename {"/Users/juliettekouidri/Documents/Reuben/Projects/Cpp/AoC22/day6/day6_test.txt"};
+    std::ifstream file;
 
     file.open(filename);
     if (file.fail()){
-        std::cout << "Failed to open file " << filename << '\n';
+        std::cout << "nope\n";
         return 1;
     }
 
-    //std::string chars = "[]";
-    //std::string line {};
 
-    //while (std::getline(file, line))
-    //{
-    //    std::stringstream ss;
-    //    ss << line;
-    //    for (const auto c: chars) {
-    //        std::replace(line.begin(), line.end(), c, ' ');
-    //    }
-    //    std::cout << line << '\n';
-    //}
+    std::string s {};
+    constexpr int filterLength { 4 };
 
-    //file.close();
+    for (std::string line; std::getline(file, line); ){
+        std::cout << line;
+    }
+    while (std::getline(file, s))
+    {
+        for (int i = 0; i < s.length() - filterLength; i++)
+        {
+            std::string ss = s.substr(i, filterLength);
+            std::cout << ss << '\n';
+            if (!repeat(ss))
+            {
+                std::cout << i + filterLength << '\n';
+                break;
+            }
+        }
+    }
+
+    file.close();
     return 0;
-}
+};
